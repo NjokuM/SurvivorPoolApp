493931fc01926b56b139e458edd469526b431e61
Michael <michaelnjoku1200@gmail.com>
2025-11-06 15:31:36 +0000

    Included pool router

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,7 @@
 from app.routers import competition_router
 from app.routers import auth_router
 from app.routers import user_router
+from app.routers import pool_router
 from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
 from starlette.middleware.sessions import SessionMiddleware
@@ -14,6 +15,7 @@ app.add_middleware(SessionMiddleware, secret_key=SECRET_KEY)
 app.include_router(user_router.router)
 app.include_router(auth_router.router)
 app.include_router(competition_router.router)
+app.include_router(pool_router.router)
 
 app.add_middleware(
     CORSMiddleware,
